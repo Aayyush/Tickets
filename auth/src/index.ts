d764d04926d52d03c3f1d0ae847212c3965f62ef
Aayush Gupta <aayush.gupta@bison.howard.edu>
2020-05-16 01:13:57 -0400

    Added User model.

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
+import mongoose from 'mongoose';
+
 import { currentuserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
 import { errorHandler } from "./middlewares/error-handler";
 import { NotFoundError } from "./errors/not-found-error";
+
 const app = express();
 app.use(json());
 
@@ -21,6 +24,21 @@ app.all("*", async () => {
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000!!!");
-});
+const start = async () => {
+  try{
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    });
+    console.log('Connected to MongoDb');
+  } catch(err) {
+    console.log(err);
+  }
+
+  app.listen(3000, () => {
+    console.log("Listening on port 3000!!!");
+  });
+}; 
+
+start();
